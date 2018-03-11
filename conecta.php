1047f4f0f88685451b8cbc23f943d55bbf9a32fa
Priscilla <priscilla.valciano@hotmail.com>
2018-03-10 23:43:09 -0300

    comecando a criar pag login

--- conecta.php
+++ conecta.php
@@ -1,2 +1,3 @@
 <?php
 $conexao = mysqli_connect("localhost", "root", "", "loja");
+#$conexao = mysqli_connect("mysql.hostinger.com.br", "u402248314_loja", "lojaloja", "u402248314_loja");
--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 
 			<h1>Bem vindo!</h1>			
 
+			<h2>Login</h2>
+			<table>
+				<tr>
+					<td>Email</td>
+					<td><input type="email" name="email"</td>
+				</tr>
+			</table>
 <?php include("rodape.php"); ?>
